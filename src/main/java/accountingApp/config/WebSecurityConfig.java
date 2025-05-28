305fdc39d14d1e8afeaef04f707d7d93e799f973
klimenkoMA <klimenko.ma.1982@gmail.com>
2025-05-28 05:19:37 +0300

    Created some files

--- src/main/java/accountingApp/config/WebSecurityConfig.java
+++ src/main/java/accountingApp/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import accountingApp.securityController.SecurityControllerClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,45 +19,44 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        synchronized (http) {
-            http
-                    .authorizeRequests()
-                    .antMatchers("/resources/**", "/static/**", "/css/**", "/video/**", "/js/**").permitAll()
-                    .antMatchers("/feedbacks").hasRole("ADMIN")
-                    .antMatchers("/users").hasRole("ADMIN")
-                    .anyRequest().authenticated()
-                    .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                    .logout()
-                    .permitAll()
-                    .and()
-                    .exceptionHandling()
-                    .accessDeniedHandler((request, response, accessDeniedException) -> {
-                        // Проверяем, есть ли у пользователя роль ADMIN
-                        logger.warn("WebSecurityConfig: User's role is " +
-                                request.getAuthType());
-                        if (request.isUserInRole("ADMIN")) {
-                            if (request.getRequestURI().contains("/feedbacks")) {
-                                response.sendRedirect("/feedbacks");
-                            } else if (request.getRequestURI().contains("users")) {
-                                response.sendRedirect("/users");
-                            }
 
-                        } else {
-                            response.sendRedirect("/main");
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/static/**", "/css/**",
+                        "/video/**", "/js/**").permitAll()
+                .antMatchers("/feedbacks").hasRole("ADMIN")
+                .antMatchers("/users").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    // Проверяем, есть ли у пользователя роль ADMIN
+                    logger.warn("WebSecurityConfig: User's role is " +
+                            request.getAuthType());
+                    if (request.isUserInRole("ADMIN")) {
+                        if (request.getRequestURI().contains("/feedbacks")) {
+                            response.sendRedirect("/feedbacks");
+                        } else if (request.getRequestURI().contains("users")) {
+                            response.sendRedirect("/users");
                         }
-                    })
-                    .authenticationEntryPoint((request, response, authException) -> {
-                        response.sendRedirect("/login");
-                    })
-                    .defaultAuthenticationEntryPointFor((request, response, authException) -> {
+                    } else {
                         response.sendRedirect("/main");
-                    }, new AntPathRequestMatcher("/**"))
-                    .and()
-                    .csrf().disable();
-        }
+                    }
+                })
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.sendRedirect("/login");
+                })
+                .defaultAuthenticationEntryPointFor((request, response, authException) -> {
+                    response.sendRedirect("/main");
+                }, new AntPathRequestMatcher("/**"))
+                .and()
+                .csrf().disable();
     }
 }
